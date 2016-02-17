37545fd1178cd5112b2bcc51ee1dee9befa9f93e
ivory1996 <bknashi@gmail.com>
2016-02-17 20:01:05 +0530

    valid blocks fro next move determined

--- team21.py
+++ team21.py
@@ -14,4 +14,34 @@ class Player21:
 		if old_move[0]==-1 and old_move[1]==-1:
 			return (4,4)
 
-		
+		cells=get_valid_moves(temp_board,temp_block,old_move,flag)
+
+	def get_valid_moves(self,temp_board,temp_block,old_move,flag):
+		blocks=get_valid_blocks(temp_block,old_move)
+
+	def get_valid_blocks(self,temp_block,old_move):
+		valid_blocks = []
+		if old_move[0] % 3 == 0 and old_move[1] % 3 == 0:
+			valid_blocks = [1,3]
+		elif old_move[0] % 3 == 0 and old_move[1] % 3 == 2:
+			valid_blocks = [1,5]
+		elif old_move[0] % 3 == 2 and old_move[1] % 3 == 0:
+			valid_blocks = [3,7]
+		elif old_move[0] % 3 == 2 and old_move[1] % 3 == 2:
+			valid_blocks = [5,7]
+		elif old_move[0] % 3 == 0 and old_move[1] % 3 == 1:
+			valid_blocks = [0,2]
+		elif old_move[0] % 3 == 1 and old_move[1] % 3 == 0:
+			valid_blocks = [0,6]
+		elif old_move[0] % 3 == 2 and old_move[1] % 3 == 1:
+			valid_blocks = [6,8]
+		elif old_move[0] % 3 == 1 and old_move[1] % 3 == 2:
+			valid_blocks = [2,8]
+		elif old_move[0] % 3 == 1 and old_move[1] % 3 == 1:
+			valid_blocks = [4]
+
+		final_valid_blocks = []
+		for i in valid_blocks:
+			if temp_block[i] == '-':
+				final_valid_blocks.append(i)
+		return final_valid_blocks
