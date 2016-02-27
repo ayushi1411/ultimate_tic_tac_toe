dec4a325086d619bd408c59c0d97a8042b992372
shreya_03 <shreyajainryp@gmail.com>
2016-02-28 02:25:12 +0530

    heuristic with depth 2

--- team21.py
+++ team21.py
@@ -5,10 +5,11 @@ Shreya Jain 201402230
 Class Player21 implemented using alpha beta pruning and other heuristics
 '''
 import copy
+import random
 class Player21:
 	def __init__(self):
 		self.WINNING_SEQUENCE=[(0,1,2),(3,4,5),(6,7,8),(0,4,8),(2,4,6),(0,3,6),(1,4,7),(2,5,8)]
-		self.DEPTH=4
+		self.DEPTH=2
 		self.maximum=9223372036854775807
 
 	MAXX = 9223372036854775807
@@ -18,17 +19,19 @@ class Player21:
 			return (4,4)
 
 		cells=self.get_valid_moves(temp_board,temp_block,old_move,flag)
-		print cells
+#		print cells
 		if len(cells)==1:
 			return (cells[0][0],cells[0][1])
 
 		action_values=[]
 		for action in cells:
 			successor_state = self.generate_successor(temp_board, action, flag)
-			action_values.append((action, self.__min_val_ab(successor_state, self.DEPTH, temp_block, flag, old_move)))
+#			print 'action -> ',action,' successor_state -> ',successor_state
+			action_values.append((action, self.__min_val_ab(successor_state, self.DEPTH, temp_block, flag, action)))
+#			print 'iamaction'
 
 		_, best_action_val = max(action_values, key=lambda x: x[1])
-		print "out"
+		print "outmove"
 		final_choice=[]
 		for best_action,value in action_values:
 			if value==best_action_val:
@@ -49,8 +52,8 @@ class Player21:
 			temp=[]
 			temp=self.get_valid_cells(temp_board,i,flag)
 
-			for i in temp:
-				cells.append(i)
+			for j in temp:
+				cells.append(j)
 
 		valid_cells=[]
 		#print cells
@@ -190,59 +193,106 @@ class Player21:
 
 		else :
 			return -1
+	
+	def get_winner(self,block):
+		if block[0] == block[1] and block[1] == block[2] and block[1] != '-':
+			return block[0]
+		elif block[3] == block[4] and block[4] == block[5] and block[4] != '-':
+			return block[3]
+		elif block[6] == block[7] and block[7] == block[8] and block[7] != '-':
+			return block[6]
+		elif block[0] == block[3] and block[3] == block[6] and block[3] != '-':
+			return block[0]
+		elif block[1] == block[4] and block[4] == block[7] and block[4] != '-':
+			return block[1]
+		elif block[2] == block[5] and block[5] == block[8] and block[5] != '-':
+			return block[2]
+		elif block[0] == block[4] and block[4] == block[8] and block[4] != '-':
+			return block[0]
+		elif block[2] == block[4] and block[4] == block[6] and block[4] != '-':
+			return block[2]
+		else:
+			return False
 
 	def __min_val_ab(self,temp_board, depth, temp_block, flag, old_move, alpha=-(MAXX), beta=(MAXX)):	
-		score=[0]*9
+		score=0
+#		print 'min func in'
+#		if self.terminal_test(temp_board, depth, temp_block):
 		if self.terminal_test(temp_board, depth, temp_block):
-			for i in range(9):
-				block_state=[]
-				for j in range((i/3)*3,(i/3)*3+3):
-					for k in range((i%3)*3,(i%3)*3+3):
-						block_state.append(temp_board[j][k])
-				score[i]=self.evaluation_func(block_state, flag)
-
+			if self.get_winner(temp_block)!=False:
+				return self.evaluation_func(temp_block,flag)
+			elif depth==0:
+				for i in range(9):
+					block_state=[]
+					for j in range((i/3)*3,(i/3)*3+3):
+						for k in range((i%3)*3,(i%3)*3+3):
+							block_state.append(temp_board[j][k])
+					score=score+self.evaluation_func(block_state, flag)
+				return score
+				
+#		print score
 		val = (self.maximum)
 		for act in self.get_valid_moves(temp_board,temp_block,old_move,flag):
-			successor_state = self.generate_successor(temp_board, act, flag)
-			val = min(val, self.__max_val_ab(successor_state,  depth - 1, temp_block, flag, old_move, alpha, beta))
+			successor_state = self.generate_successor(temp_board, act, self.opponent(flag))
+#			print 'action-> ', act, ' successor_state-> ', successor_state
+			val = min(val, self.__max_val_ab(successor_state,  depth - 1, temp_block,flag, act, alpha, beta))
+#		print "alpha -> ",alpha," beta -> ",beta ," val ->",val
 			if val <= alpha:
+#				print 'min out 1'
 				return val
 			beta = min(beta, val)
+#		print 'min out 2'
 		return val
 
 	def __max_val_ab(self,temp_board, depth, temp_block,flag, old_move, alpha=-(MAXX), beta=(MAXX)):
-		score=[0]*9
+		score=0
+#		print 'max in'
 		if self.terminal_test(temp_board, depth, temp_block):
-			for i in range(9):
-				block_state=[]
-				for j in range((i/3)*3,(i/3)*3+3):
-					for k in range((i%3)*3,(i%3)*3+3):
-						block_state.append(temp_board[j][k])
-				score[i]=self.evaluation_func(block_state, flag)
-
+			if self.get_winner(temp_block)!=False:
+				return self.evaluation_func(temp_block,flag)
+			elif depth==0:
+				for i in range(9):
+					block_state=[]
+					for j in range((i/3)*3,(i/3)*3+3):
+						for k in range((i%3)*3,(i%3)*3+3):
+							block_state.append(temp_board[j][k])
+					score=score+self.evaluation_func(block_state, flag)
+				return score
+#	print score
 		val = -(self.maximum)
+		temp=self.get_valid_moves(temp_board,temp_block,old_move,flag)
+#		print ' list of valid moves' ,temp
 		for act in self.get_valid_moves(temp_board,temp_block,old_move,flag):
+			
 			successor_state = self.generate_successor(temp_board, act, flag)
-			val = max(val, self.__min_val_ab(successor_state, depth, temp_block, flag, old_move, alpha, beta))
+#			print 'action-> ', act, ' successor_state-> ', successor_state
+			val = max(val, self.__min_val_ab(successor_state, depth, temp_block,flag, act, alpha, beta))
+#			print "alpha -> ",alpha," beta -> ",beta," val -> ",val
 			if val >= beta:
+#				print 'max out 1'
 				return val
 			alpha = max(alpha, val)
+#		print 'max out 2'
 		return val
 
 
 	def terminal_test(self,temp_board, depth, temp_block):
+#		print 'terminal test'
 		if depth==0:
+#			print 'terminal reached'
 			return True
 		a,msg =  self.terminal_state_reached(temp_board, temp_block)
+#		print 'message-> ',msg
 		return a
 
 	def generate_successor(self, temp_board, action, flag):
+#		print 'generate_successor'
 		board = copy.deepcopy(temp_board)
 		board[action[0]][action[1]] = flag
 		return board
 
 	def terminal_state_reached(self,game_board, status_block):
-		
+#		print 'terminal_state_reached'
 		boardstat = status_block
 		if (boardstat[0] == boardstat[1] and boardstat[1] == boardstat[2] and boardstat[1]!='-' and boardstat[1]!='d') or (boardstat[3]!='d' and boardstat[3]!='-' and boardstat[3] == boardstat[4] and boardstat[4] == boardstat[5]) or (boardstat[6]!='d' and boardstat[6]!='-' and boardstat[6] == boardstat[7] and boardstat[7] == boardstat[8]):
 			#print block_stat
@@ -301,6 +351,7 @@ class Player21:
 			return 'x'
 
 	def get_score(self,cells_seq,flag):
+#		print 'get_score'
 		score=0
 		if (cells_seq[0]==cells_seq[1] and cells_seq[1]==cells_seq[2]):
 			if cells_seq[1]==flag:
@@ -346,6 +397,7 @@ class Player21:
 		return score
 
 	def evaluation_func(self,block_state,flag):
+#		print 'evaluate function'
 		score=0
 		cells_seq=[block_state[0],block_state[1],block_state[2]]
 		score=score+self.get_score(cells_seq,flag)
